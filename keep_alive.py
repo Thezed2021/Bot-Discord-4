fa0ae21701b8e0bc6a391ef552020f716b9b40c6
Thezed2021 <86328377+Thezed2021@users.noreply.github.com>
2026-08-09 19:26:06 -0400

    sintaxe 7

--- keep_alive.py
+++ keep_alive.py
@@ -16,10 +16,9 @@ def home():
     def run():
         # Pega a porta do ambiente (Render injeta a variável PORT automaticamente)
             port = int(os.environ.get("PORT", 8080))
-                app.run(host='0.0.0.0', port=port)
+            app.run(host='0.0.0.0', port=port)
 
                 def keep_alive():
                     """Inicia o servidor Flask em uma Thread daemon"""
                         t = Thread(target=run, daemon=True)
                             t.start()
-                            
